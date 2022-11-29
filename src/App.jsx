537699fe1ce38dd6dd16879c01f6e54661004be8
Bruno Goniadis <brunogoniadislima@gmail.com>
2022-11-29 00:42:03 -0300

    componentsinterface update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
+import { MainContainer } from './styles/App';
+import { CoinContainer } from './styles/App';
 
 function App() {
   const [currencyValue, setcurrencyValue] = useState("")
 
 
-  const getApiCurrencyValue = (()  => {
+  const getApiCurrencyValue = (() => {
     axios.get("https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL").then
-     ((respose) => { 
-       setcurrencyValue(respose.data)
-     })
-     .catch(()=>{
-      setcurrencyValue(respose.data)
-     })
-     console.log(currencyValue)
- })
+      ((respose) => {
+        setcurrencyValue(respose.data)
+      })
+      .catch(() => {
+        setcurrencyValue(respose.data)
+      })
+    console.log(currencyValue)
+  })
 
   useEffect(() => {
     getApiCurrencyValue()
   }, [])
 
-  if(currencyValue==""){
-    return(
+  if (currencyValue == "") {
+    return (
       <h1>carregando moedas</h1>
     )
-  }else{
+  } else {
     return (
-      <div className="App">
-        <h3>{currencyValue["USDBRL"] ["code"]}</h3>
-        <h3>{currencyValue["USDBRL"] ["high"]}</h3>
-      </div>
+      <MainContainer>
+
+        <CoinContainer>
+          <h3>{currencyValue["USDBRL"]["code"]}</h3>
+          <h3>{currencyValue["USDBRL"]["high"]}</h3>
+        </CoinContainer>
+        <CoinContainer>
+          <h3>{currencyValue["EURBRL"]["code"]}</h3>
+          <h3>{currencyValue["EURBRL"]["high"]}</h3>
+        </CoinContainer>
+
+      </MainContainer>
     )
   }
 
---
+++ src/styles/App.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+` 
+export const CoinContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin:10px;
+`
